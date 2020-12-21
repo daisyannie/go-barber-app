2d05fe24630c28fa92fe88e2a2b7adc546a26534
Daisy <daisy@vizir.com.br>
2020-12-21 09:55:53 -0300

    test

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
+
+describe('SignIn page', () => {
+  it('should contains email/password inputs', () => {
+    const { getByPlaceholder } = render(<SignIn />);
+
+    expect(getByPlaceholder('E-mail')).toBeTruthy();
+    expect(getByPlaceholder('Senha')).toBeTruthy();
+  });
+});
